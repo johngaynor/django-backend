4fea4fab9af054a97cdc041e1888ce6a60e08b6a
johngaynor <86327580+johngaynor@users.noreply.github.com>
2023-09-18 20:51:42 -0400

    added template for keys

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,25 @@ from django.urls import path
 from . import views
 # map URL's to functions
 
+# temporary storage for keys
+gasKeys = ['BP', 'Speedway', 'SPEEDWAY']
+travelKeys = ['VALVOLINE', 'IPARK']
+groceryKeys = ['KROGER', 'MEIJER']
+recKeys = ['SUBWAY', 'BLAZE', 'CHICK FIL A', 'DAIRY QUEEN',
+           'HACIENDA', 'JIMMY JOHNS', 'SHAKE SHACK']
+schoolKeys = []
+leisureKeys = ['AMAZON', 'AUSBLAKE', 'INDY CITY', 'GOOGLE']
+rentKeys = []
+miscKeys = ['APPLE.COM', 'MISCELLANEOUS DEBIT']
+incomeKeys = ['IRVING', 'INDIANA UNV CONS CP']
+ignoreKeys = ['TRANSFER', 'DISCOVER']
+
+keys = [gasKeys, travelKeys, groceryKeys, recKeys, schoolKeys,
+        leisureKeys, rentKeys, miscKeys, incomeKeys, ignoreKeys]
+
 # url configuration, always end routes with '/'
 urlpatterns = [
     path('', views.home, name='home'),  # root path URL
     path('hello/', views.say_hello),
-    path('autocategory/', views.auto_category)
+    path('autocategory/', views.auto_category, {'keys': keys})
 ]
